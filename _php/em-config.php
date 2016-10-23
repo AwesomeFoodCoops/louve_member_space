74cd4766d73074b36c17bc12f36c0b4f0e0a39de
shewolf <shewolf@testserver>
2016-10-23 11:54:36 +0200

    Utilise des constantes pour gérer le chemins dans les fichiers inclus. Utilisé dans head.php et à généraliser.

---
+++ _php/em-config.php
@@ -0,0 +1,18 @@
+<?php
+/* Constantes pour l'espace membre 
+ * prefix LEM (Louve Espace Membres) 
+ */
+class EmConfig
+{
+	/* Racine du site web de l'espace membre:
+	 * Exemple, pour http://public.cooplalouve.fr/espace-membre/
+	 * il faudrait mettre "/espace-membres/"
+	 * -
+	 * Cette constante est à utiliser pour les chemins vers des resources, 
+	 * CSS par exemple,  dans les fichiers qui seront inclus depuis différents
+	 * chemins.
+	 * Il n'y a pas de '/' final pour qu'elle se comporte comme __DIR__
+	 */
+	const ROOT = "";
+}
+?>
--- _php/head.php
+++ _php/head.php
@@ -1,14 +1,14 @@
 <?php
-require("_php/session.php");
-
+    require(__DIR__."/session.php");
+    require(__DIR__."/em-config.php");
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>La louve - mon espace</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/bootgrid.css">
+    <link rel="stylesheet" type="text/css" href="<?=EmConfig::ROOT?>/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?=EmConfig::ROOT?>/css/bootgrid.css">
     <link href='https://fonts.googleapis.com/css?family=Raleway:400,700,900,300' rel='stylesheet' type='text/css'>
     <meta charset="UTF-8">
     <style type="text/css">
