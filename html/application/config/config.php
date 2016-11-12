fe6fb6eea5176255403d6df479f5d93404b19b99
Julien Zorko <julien@zorko.fr>
2016-11-12 01:33:03 +0100

    changed macro for URL so it works when the site is in a subfolder.

--- html/application/config/config.php
+++ html/application/config/config.php
@@ -50,11 +50,11 @@ else {
  * then replace this line with full URL (and sub-folder) and a trailing slash.
  */
 
-define('URL_PUBLIC_FOLDER', 'public');
+define('URL_PUBLIC_FOLDER', '/public');
 define('URL_PROTOCOL', '//');
 define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
-define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
-define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
+define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME']).'/'));
+define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER );
 
 /**
  * Configuration for: Database
