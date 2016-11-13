4056ba4f3cb64ded45dd0eb1ee4eb1c614f6dd34
MicroLink26 <mic.roche@gmail.com>
2016-11-13 13:09:44 +0100

    add emergencies view

---
+++ html/application/view/emergencies/index.php
@@ -0,0 +1,18 @@
+
+<div class="container">
+<?php
+    $currentsEmergency = $emergency->getCurrents();
+
+
+   
+    foreach ( $currentsEmergency as $currentEmergency) :
+
+?>
+
+<div class="alert alert-info fade in">
+    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <strong> <?php echo  $currentEmergency["titre"];?> : </strong>
+    <a href="<?php echo  $currentEmergency["lien"];?>"> <?php echo  $currentEmergency["info"];?> </a>
+</div>
+<?php endforeach; ?>
+</div>
