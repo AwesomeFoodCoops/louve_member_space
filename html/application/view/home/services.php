722de3b89ffb46b21eac719ba6d8059f73ae5d7f
Julien Zorko <julien@zorko.fr>
2016-11-20 00:58:00 +0100

    remplacement de 'créneau(x)' par 'service(s)', mais il en reste...

--- html/application/view/home/services.php
+++ html/application/view/home/services.php
@@ -2,17 +2,17 @@
     <div class="row">
         <div class="col-xs-12 col-sm-6">
             <div class="louve-creneau">
-                <h3><strong>Consulter les crénaux volants</strong></h3>
-                <p> Vous êtes en équipe volante et souhaitez savoir quels sont les crénaux disponibles pour vous inscrire ? Consultez les ici avant de contacter le bureau des membres. </p>
+                <h3><strong>Consulter les services volants</strong></h3>
+                <p> Vous êtes en équipe volante et souhaitez savoir quels sont les services disponibles pour vous inscrire ? Consultez les ici avant de contacter le bureau des membres. </p>
                 <a href="<?php echo URL . 'shift/ftopshifts/'; ?>">
-                <button class="btn btn-default" style="border-color : green; color: green;" ><span class="glyphicon glyphicon-ok"></span>Je regarde les crénaux.</button>
+                <button class="btn btn-default" style="border-color : green; color: green;" ><span class="glyphicon glyphicon-ok"></span>Je regarde les services.</button>
                 </a>
             </div>
         </div>
         <div class="col-xs-12 col-sm-6">
             <div class="louve-creneau">
-                <h3><strong>Echanger son créneau</strong></h3>
-                <p> Vous serez indisponible pour effectuer votre prochain créneau ? Vous pouvez tenter de l'échanger ici avec un autre membre de la louve. </br></p>
+                <h3><strong>Echanger son service</strong></h3>
+                <p> Vous serez indisponible pour effectuer votre prochain service ? Vous pouvez tenter de l'échanger ici avec un autre membre de la louve. </br></p>
                 <button class="btn btn-default disabled" style="border-color : red; color : red;"><span class="glyphicon glyphicon-retweet"></span> Prochainement ici.</button>
             </div>
         </div>
