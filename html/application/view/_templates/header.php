87ca8fdc4e5a86e8a43e6710c866d9e608732dcd
Julien Zorko <julien@zorko.fr>
2016-11-20 21:36:51 +0100

    remove target="_blank" from FORUM link

--- html/application/view/_templates/header.php
+++ html/application/view/_templates/header.php
@@ -9,7 +9,7 @@
          +> faire un import uniquement pour ces pages -->
         <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/easyui.css">
         <link rel="stylesheet" type="text/css" href="<?php echo URL; ?>css/icon.css">
-        <!-- 
+        <!--
         Lien vers les differentes favicons
         -->
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
@@ -48,16 +48,16 @@
 
         <div class="nav navbar-nav collapse navbar-collapse" id="louvenav">
             <li><a href="<?php echo URL . 'home/participation/'; ?>"><span class="glyphicon glyphicon-time" style="color:grey"></span> MA PARTICIPATION</a></li>
-			<li><a href="<?php echo URL . 'home/services/'; ?>"><span class="glyphicon glyphicon-ok" style="color:grey"></span> SERVICES</a></li>
-            <li><a href="https://membres.cooplalouve.fr/forum" target="_blank"><span class="glyphicon glyphicon-earphone" style="color:grey"></span> FORUM</a></li>
-            <?php 
-			if($GLOBALS['User']->isAdmin())
-					echo(' <li><a href="'.URL . 'management"><span class="glyphicon glyphicon-plus" style="color:grey"></span> GESTION </a></li>');
+            <li><a href="<?php echo URL . 'home/services/'; ?>"><span class="glyphicon glyphicon-ok" style="color:grey"></span> SERVICES</a></li>
+            <li><a href="<?php echo URL . 'forum'; ?>"><span class="glyphicon glyphicon-earphone" style="color:grey"></span> FORUM</a></li>
+            <?php
+            if($GLOBALS['User']->isAdmin())
+                    echo(' <li><a href="'.URL . 'management"><span class="glyphicon glyphicon-plus" style="color:grey"></span> GESTION </a></li>');
             if ($GLOBALS['hasEmergency']) {
                 $emergencyStyle = "color:lightcoral";
-				echo '<li><a href="' . URL . 'emergency/" style="' . $emergencyStyle . ';"><span class="glyphicon glyphicon-alert urgences"></span> URGENCES</a></li>';
+                echo '<li><a href="' . URL . 'emergency/" style="' . $emergencyStyle . ';"><span class="glyphicon glyphicon-alert urgences"></span> URGENCES</a></li>';
             }
-          
+
         ?>
         </div>
         <ul class="nav navbar-inverse navbar-nav navbar-right">
