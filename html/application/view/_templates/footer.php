78494a361d860f8e020aa7b31b4808216d6cae26
Zied Kheriji <zied.web@gmail.com>
2017-01-22 19:29:24 +0100

    adjusting style attibutes (some should be moved to CSS)

--- html/application/view/_templates/footer.php
+++ html/application/view/_templates/footer.php
@@ -1,63 +1,72 @@
     <!-- TODO_LATER Utile pour nous ? -->
     <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
-   
-<div id="footer">
+  
+<div class="footer-bottom">
   <div class="container">
-    <div class="row">
-    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
-    		<h4><a href='https://membres.cooplalouve.fr/home/participation/'>MES SERVICES</a></h4>
-    		<p>Agenda des services</p>
-    		<p>Contact Coordinateur</p>
-    		<p>Bureau des membres</p>
-    		<p>Planning des services</p>
-    	</div>
-    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
-    		<h4><a href='https://membres.cooplalouve.fr/home/services/'>OUTILS</a></h4>
-    		<p>Services Volants</p>
-    		<p>Echanger un créneau</p>
-    		<p>Organiser un covoiturage</p>
-    	</div>
-    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
-    		<h4><a href='https://membres.cooplalouve.fr/forum/'>FORUM</a></h4>
-    		<p>Discussion La Louve</p>
-    		<p>Discussion de comptoir</p>
-    		<p>Recettes autour des produits</p>
-    		<p>Evenements et Rencontres</p>
-    		<p>Petites Annonces</p>
-    		<p>Echange de créneaux</p>
-    		<p>Covoiturages</p>
-    		<p>F.À.Q</p>
-    		<p>Plateforme Web</p>
-    	</div>
-    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
-    		<h4><a href=''>VIE DE LA LOUVE</a></h4>
-    		<p>Besoins urgents</p>
-    		<p>Prochaine AG</p>
-    		<p>Louveletter</p>
-    		<p>Chiffres Clés</p>
-    		<p>Agenda</p>
-    	</div>
-    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
-    		<h4><a href=''>DOCUMENTS</a></h4>
-    		<p>Guide du Supermarché</p>
-    		<p>Compte Rendu AG</p>
-    		<p>Organisation</p>
-    	</div>
-    	<div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
-    		<h4><a href='https://membres.cooplalouve.fr/'>INFOS</a></h4>
-    		<p>116 rue des poissonniers</p>
-    		<p>75018 Paris</p>
-    		<p>info@cooplalouve.fr</p>
-    		<p>06 80 80 80 80</p>
-    		<p>Facebook Privé</p>
-    	</div>
+    <div class="row" style='display:flex; flex-wrap: wra'>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
+            <h4><a href='https://membres.cooplalouve.fr/home/participation/'>MES SERVICES</a></h4>
+            <ul class="list-unstyled clear-margins">
+                <li>Agenda des services</li>
+                <li>Contact Coordinateur</li>
+                <li>Bureau des membres</li>
+                <li>Planning des services</li>
+            </ul>
+            
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
+            <h4><a href='https://membres.cooplalouve.fr/home/services/'>OUTILS</a></h4>
+            <ul class="list-unstyled clear-margins"><li>Services Volants</li>
+            <li>Echanger un créneau</li>
+            <li>Organiser un covoiturage</li>
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
+            <h4><a href='https://membres.cooplalouve.fr/forum/'>FORUM</a></h4>
+            <ul class="list-unstyled clear-margins"><li>Discussion La Louve</li>
+            <li>Discussion de comptoir</li>
+            <li>Recettes autour des produits</li>
+            <li>Evenements et Rencontres</li>
+            <li>Petites Annonces</li>
+            <li>Echange de créneaux</li>
+            <li>Covoiturages</li>
+            <li>F.À.Q</li>
+            <li>Plateforme Web</li>
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
+            <h4><a href=''>VIE DE LA LOUVE</a></h4>
+            <ul class="list-unstyled clear-margins"><li>Besoins urgents</li>
+            <li>Prochaine AG</li>
+            <li>Louveletter</li>
+            <li>Chiffres Clés</li>
+            <li>Agenda</li>
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
+            <h4><a href=''>DOCUMENTS</a></h4>
+            <ul class="list-unstyled clear-margins">
+            <li>Guide du Supermarché</li>
+            <li>Compte Rendu AG</li>
+            <li>Organisation</li>
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 ">
+            <h4><a href='https://membres.cooplalouve.fr/'>INFOS</a></h4>
+            <ul class="list-unstyled clear-margins"><li>116 rue des poissonniers</li>
+            <li>75018 Paris</li>
+            <li>info@cooplalouve.fr</li>
+            <li>06 80 80 80 80</li>
+            <li>Facebook Privé</li>
+            </ul>
+        </div>
     </div>
   </div>
 </div>
 
-
+<!-- 
     <script>
         var url = "<?php echo URL; ?>";
-    </script>
+    </script> -->
 </body>
 </html>
