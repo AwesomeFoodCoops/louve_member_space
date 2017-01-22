19f173c15a1303bd5993083304211d9e2b10b193
Zied Kheriji <zied.web@gmail.com>
2017-01-22 18:34:23 +0100

    adding sitemap to footer.php

--- html/application/view/_templates/footer.php
+++ html/application/view/_templates/footer.php
@@ -1,5 +1,61 @@
     <!-- TODO_LATER Utile pour nous ? -->
     <!-- define the project's URL (to make AJAX calls possible, even when using this in sub-folders etc) -->
+   
+<div id="footer">
+  <div class="container">
+    <div class="row">
+    	<div class="col-xs-12 col-sm-4 col-md-2 col-lg-1 ">
+    		<h4>MES SERVICES</h4>
+    		<p>Agenda des services</p>
+    		<p>Contact Coordinateur</p>
+    		<p>Bureau des membres</p>
+    		<p>Planning des services</p>
+    	</div>
+    	<div class="col-xs-12 col-sm-4 col-md-2 col-lg-1 ">
+    		<h4>OUTILS</h4>
+    		<p>Services Volants</p>
+    		<p>Echanger un créneau</p>
+    		<p>Organiser un covoiturage</p>
+    	</div>
+    	<div class="col-xs-12 col-sm-4 col-md-2 col-lg-1 ">
+    		<h4>FORUM</h4>
+    		<p>Discussion La Louve</p>
+    		<p>Discussion de comptoir</p>
+    		<p>Recettes autour des produits</p>
+    		<p>Evenements et Rencontres</p>
+    		<p>Petites Annonces</p>
+    		<p>Echange de créneaux</p>
+    		<p>Covoiturages</p>
+    		<p>F.À.Q</p>
+    		<p>Plateforme Web</p>
+    	</div>
+    	<div class="col-xs-12 col-sm-4 col-md-2 col-lg-1 ">
+    		<h4>VIE DE LA LOUVE</h4>
+    		<p>Besoins urgents</p>
+    		<p>Prochaine AG</p>
+    		<p>Louveletter</p>
+    		<p>Chiffres Clés</p>
+    		<p>Agenda</p>
+    	</div>
+    	<div class="col-xs-12 col-sm-4 col-md-2 col-lg-1 ">
+    		<h4>DOCUMENTS</h4>
+    		<p>Guide du Supermarché</p>
+    		<p>Compte Rendu AG</p>
+    		<p>Organisation</p>
+    	</div>
+    	<div class="col-xs-12 col-sm-4 col-md-2 col-lg-1 ">
+    		<h4>INFOS</h4>
+    		<p>116 rue des poissonniers</p>
+    		<p>75018 Paris</p>
+    		<p>info@cooplalouve.fr</p>
+    		<p>06 80 80 80 80</p>
+    		<p>Facebook Privé</p>
+    	</div>
+    </div>
+  </div>
+</div>
+
+
     <script>
         var url = "<?php echo URL; ?>";
     </script>
