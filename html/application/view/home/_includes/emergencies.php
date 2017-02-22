8f225bc43f08b8551f3757df238ffc2638234015
ZiedKhe <ZiedKhe@users.noreply.github.com>
2017-02-22 18:34:14 +0100

    text centered in the box

--- html/application/view/home/_includes/emergencies.php
+++ html/application/view/home/_includes/emergencies.php
@@ -3,7 +3,7 @@ if( $GLOBALS['hasEmergency'])
 {
     $currentEmergency = $emergency->getCurrent();
 ?>
-<div class="alert alert-danger fade in">
+<div class="alert alert-danger fade in text-centered">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <span><strong> <?php echo $currentEmergency->titre;?> : </strong>
     <a href="<?php echo $currentEmergency->lien;?>"> <?php echo $currentEmergency->info;?> </a></span>
