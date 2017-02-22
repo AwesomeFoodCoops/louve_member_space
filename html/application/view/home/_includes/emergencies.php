56679fec9952b4fad3f6de63c5dad562df634878
ZiedKhe <ZiedKhe@users.noreply.github.com>
2017-02-22 18:28:47 +0100

    CSS rules applied on emergencies - homepage

--- html/application/view/home/_includes/emergencies.php
+++ html/application/view/home/_includes/emergencies.php
@@ -5,8 +5,8 @@ if( $GLOBALS['hasEmergency'])
 ?>
 <div class="alert alert-danger fade in">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong> <?php echo $currentEmergency->titre;?> : </strong>
-    <a href="<?php echo $currentEmergency->lien;?>"> <?php echo $currentEmergency->info;?> </a>
+    <span><strong> <?php echo $currentEmergency->titre;?> : </strong>
+    <a href="<?php echo $currentEmergency->lien;?>"> <?php echo $currentEmergency->info;?> </a></span>
 </div>
 <?php
 }
