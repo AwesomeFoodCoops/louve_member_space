25e9e823e99970e07cf08182cdc53fbd6b4dd2a3
ZiedKhe <ZiedKhe@users.noreply.github.com>
2017-02-22 18:21:40 +0100

    UI : emergencies are now displayed in red "alert-danger"

--- html/application/view/emergencies/index.php
+++ html/application/view/emergencies/index.php
@@ -10,7 +10,7 @@
 
 ?>
 
-<div class="alert alert-info fade in">
+<div class="alert alert-danger fade in">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <strong> <?php echo  $currentEmergency["titre"];?> : </strong>
     <a href="<?php echo  $currentEmergency["lien"];?>"> <?php echo  $currentEmergency["info"];?> </a>
