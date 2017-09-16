f245125c5ee2a910e8ff1c7da65cd08ae4fb60e4
Mike <mic.roche@gmail.com>
2017-09-16 10:46:59 +0200

    Add files via upload

--- html/application/view/home/my_info.php
+++ html/application/view/home/my_info.php
@@ -14,9 +14,11 @@ $display = $user->getStatusDisplay();
             } else {
                
                 echo ('<h4> Bonjour '. $user->getFirstname() . ' ' . $user->getLastname() .'.</h4>');
-                echo ('<p><strong>Votre numéro de téléphone :</strong> '. $user->getPhone() .' . <p>');
-                echo ('<p> <strong>Votre e-mail de contact :</strong> '. $user->getEmail() .'. <p>');
-                echo ('<p> <strong>Votre numéro de membre : </strong> '. $user->getId() .'. <p>');
+                echo ('<p><strong>Votre numéro de téléphone :</strong> '. $user->getPhone() .'  </p>');
+                echo ('<p> <strong>Votre e-mail de contact :</strong> '. $user->getEmail() .' </p>');
+                echo ('<p> <strong>Votre numéro de membre :</strong> '. $user->getId() .' </p>');
+               
+                //echo ('<p><strong>Compteur vacations :</strong> '. $user->getFinal_ftop_point() .'</p>');
                 echo ('<p><strong>'.$display['alert_msg'].'</strong> '.$display['full_msg'].'</p>');
                 echo ('<p> <br/><strong>Pour modifier ces informations contactez le bureau des membres. </strong><p>');
 		echo ('<a href="' . URL . '/selfservice">
