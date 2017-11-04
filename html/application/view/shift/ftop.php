cb9395e0273219f1df3cb1c467203db3358a0a9d
Mike <mic.roche@gmail.com>
2017-11-04 18:14:12 +0100

    Update ftop.php

--- html/application/view/shift/ftop.php
+++ html/application/view/shift/ftop.php
@@ -1,6 +1,6 @@
 <div class="container">
   
-<!--
+
     <div class="row">
         <div class="col-lg-12">
             <h3 class=" ui horizontal divider"><strong>Mes infos</strong></h3>
@@ -15,7 +15,7 @@
             </div>
         </div>
     </div>
--->
+
       <div class="row">
     <div class="col-xs-12">
         <h3  class="entete ui horizontal divider"><strong>Services volants disponibles</strong></h3>
