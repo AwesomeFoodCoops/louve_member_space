58c9b4d6d63841dc771c62fa81bd372a404416e6
Mike <mic.roche@gmail.com>
2018-06-17 13:19:49 +0200

    Mise a jour documentation

--- html/application/view/_includes/documents.php
+++ html/application/view/_includes/documents.php
@@ -5,16 +5,14 @@
 
 		<div class="louve-box">
 
-		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/venir_a_la_louve.pdf" target="_blank">
-			<p  ><img src="img/bus.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
-				<br/>Venir au Supermarché</a></p>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/Manuel_membres_La_Louve.pdf" target="_blank">
+			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
+				<br/>Manuel des membres</a></p>
 		</div>
-		
 		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/basecoordinateurs.pdf" target="_blank">
 			<p  ><img src="img/team.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
 				<br/>Pour les coordinateurs</a></p>
 		</div>
-		
 		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" >
 			<a href="pdfs/Statuts_et_presence.pdf" target="_blank"><p  >
 				<img src="img/time.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
@@ -26,23 +24,33 @@
 				<br/>Exemple de feuille de présence</a></p>
 		</div>
 
-		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/statutsLouve.pdf" target="_blank">
-			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
-				<br/>Statuts de la coopérative</a></p>
-		</div>
 
-				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="https://docs.google.com/document/d/1RXdTmg8aFcEuv3UrD0LbnbT0LDLj7LAYuq3YCS9S0I4/edit" target="_blank">
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="https://docs.google.com/document/d/1RXdTmg8aFcEuv3UrD0LbnbT0LDLj7LAYuq3YCS9S0I4/edit" target="_blank">
 			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
 				<br/>AG et comité Ordre du Jour</a></p>
 		</div>
-				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/modelePARKSLOPE.pdf" target="_blank">
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="https://docs.google.com/document/d/1aE_3zT5kfeNMJZkDeo7_u7Hz4op4DL-kNmOtZt7xGy8/edit?usp=sharing" target="_blank">
+			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
+				<br/>Guide de sécurité incendie</a></p>
+		</div>
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/statutsLouve.pdf" target="_blank">
+			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
+				<br/>Statuts de la coopérative</a></p>
+		</div>	
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/statutsAmisLouve.pdf" target="_blank">
+			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
+				<br/>Statuts de l’association des amis de La Louve</a></p>
+		</div>		
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/modelePARKSLOPE.pdf" target="_blank">
 			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
 				<br/>Le Modèle Park Slope</a></p>
 		</div>
-				<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/Manuel_membres_La_Louve.pdf" target="_blank">
-			<p  ><img src="img/sheet.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
-				<br/>Manuel des membres</a></p>
+
+		<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="pdfs/venir_a_la_louve.pdf" target="_blank">
+			<p  ><img src="img/bus.png" style="width:80px;" alt="doc" class="img-responsive img-center" />
+				<br/>Venir au Supermarché</a></p>
 		</div>
+
         </div>
     </div>
     </div>
